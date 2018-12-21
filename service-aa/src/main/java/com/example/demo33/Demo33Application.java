39e0a3f09f6ab86d0b18495bd1727b9559041e3b
gavin <gavinaa123aah@163.com>
2018-12-21 11:46:07 +0800

    0.2  实现负载均衡

---
+++ service-aa/src/main/java/com/example/demo33/Demo33Application.java
@@ -0,0 +1,14 @@
+package com.example.demo33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo33Application.class, args);
+    }
+
+}
+
---
+++ service-aa/src/main/java/com/example/demo33/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo33;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "hello222 this is first messge";
+    }
+}
