c27f34f4acd6ca67e3961c358177bea9d900fcd6
wangming <wangming@zhenrongbao.com>
2018-12-27 00:19:23 +0800

    add img

--- quary1/src/main/java/quary1/quary1/Quary1Application.java
+++ quary1/src/main/java/quary1/quary1/Quary1Application.java
@@ -9,10 +9,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableEurekaClient
 public class Quary1Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Quary1Application.class, args);
     }
-
 }
 
